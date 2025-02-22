dad5b28bf59eb3833e198129eddec8b289395ffe
fitrat19998 <towmatovft@gmail.com>
2025-02-22 10:16:47 +0500

    modal info added fordoc creator

--- resources/views/studydepartments/documents/index.blade.php
+++ resources/views/studydepartments/documents/index.blade.php
@@ -37,12 +37,8 @@
                             <tr>
                                 <th>ID</th>
                                 <th>Hujjat turi</th>
-                                <th>Tavsif</th>
                                 <th>Fayl</th>
                                 <th>Holat</th>
-                                <th>Qaytarish sababi</th>
-                                <th>Qaytarish vaqti</th>
-                                <th>Kim qaytargan</th>
                                 <th>Chiqarilgan buyruq</th>
                                 <th>Amallar</th>
                             </tr>
@@ -52,11 +48,7 @@
                                 <tr id="datas_ids{{ $document->id }}">
                                     <td>{{ $loop->iteration }}</td>
                                     <td>{{ optional($document->documenttype)->name }}</td>
-                                    <td>
-                                        <p>{{ \Illuminate\Support\Str::limit(strip_tags($document->comment), 100, '...') }}</p>
-                                        <a href="#" class="read-more" data-content="{{ $document->comment }}"
-                                           data-title="{{ optional($document->documenttype)->name }}">To'liq o'qish</a>
-                                    </td>
+
                                     <td>
                                         @if($document->files->isNotEmpty())
                                             @foreach($document->files as $file)
@@ -113,43 +105,43 @@
                                                 @if ($done_doc)
                                                     @php
                                                    $modals[] = "
-<div class='modal fade' id='stepModal{$document->id}-{$department->id}-{$iteration}'
-     tabindex='-1' aria-labelledby='modalLabel{$document->id}-{$department->id}-{$iteration}'
-     aria-hidden='true'>
-    <div class='modal-dialog'>
-        <div class='modal-content'>
-            <div class='modal-header'>
-                <h5 class='modal-title' id='modalLabel{$document->id}-{$department->id}-{$iteration}'>
-                    Step $iteration tafsilotlari
-                </h5>
-                <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
-            </div>
-            <div class='modal-body'>
-                <p><strong>Bo'lim:</strong> {$department->name}</p>
+                                                    <div class='modal fade' id='stepModal{$document->id}-{$department->id}-{$iteration}'
+                                                         tabindex='-1' aria-labelledby='modalLabel{$document->id}-{$department->id}-{$iteration}'
+                                                         aria-hidden='true'>
+                                                        <div class='modal-dialog'>
+                                                            <div class='modal-content'>
+                                                                <div class='modal-header'>
+                                                                    <h5 class='modal-title' id='modalLabel{$document->id}-{$department->id}-{$iteration}'>
+                                                                        Tafsilotlar.
+                                                                    </h5>
+                                                                    <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+                                                                </div>
+                                                                <div class='modal-body'>
+                                                                    <p><strong>Bo'lim:</strong> {$department->name}</p>
 
-                <p><strong>Izoh:</strong> " . (($done_doc->comment !== 'cancelled' && !empty($done_doc->comment))
-                    ? ucfirst($done_doc->comment) : 'Mavjud emas') . "</p>
+                                                                    <p><strong>Izoh:</strong> " . (($done_doc->comment !== 'cancelled' && !empty($done_doc->comment))
+                                                                        ? ucfirst($done_doc->comment) : 'Mavjud emas') . "</p>
 
-                <p><strong>Ma'sul shaxs:</strong> " . ($done_doc->user ? $done_doc->user->firstname . ' ' . $done_doc->user->lastname : 'Mavjud emas') . "</p>
+                                                                    <p><strong>Ma'sul shaxs:</strong> " . ($done_doc->user ? $done_doc->user->firstname . ' ' . $done_doc->user->lastname : 'Mavjud emas') . "</p>
 
-                <p><strong>Yaratilgan vaqti:</strong> {$done_doc->updated_at->format('d.m.Y')}</p>
+                                                                    <p><strong>Yaratilgan vaqti:</strong> {$done_doc->updated_at->format('d.m.Y')}</p>
 
-                <p><strong>Qaytarilgan sababi:</strong> " . ((!empty($done_doc->report) && $done_doc->report !== 'empty')
-                    ? ucfirst($done_doc->report) : 'Mavjud emas') . "</p>
+                                                                    <p><strong>Qaytarilgan sababi:</strong> " . ((!empty($done_doc->report) && $done_doc->report !== 'empty')
+                                                                        ? ucfirst($done_doc->report) : 'Mavjud emas') . "</p>
 
-                <p><strong>Ilova qilingan fayl.</strong>
-                " . ( $document->doneuserdocs_files($done_doc->id)->isNotEmpty()
-                    ? implode('', $document->doneuserdocs_files($done_doc->id)->map(function($file) {
-                        return "<a href='" . route('doneuserdocs.download', $file->name) . "' class='btn btn-primary'>{$file->title}</a>";
-                    })->toArray())
-                    : '<p>Fayl mavjud emas.</p>') . "</p>
-            </div>
-            <div class='modal-footer'>
-                <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Yopish</button>
-            </div>
-        </div>
-    </div>
-</div>";
+                                                                    <p><strong>Ilova qilingan fayl.</strong>
+                                                                    " . ( $document->doneuserdocs_files($done_doc->id)->isNotEmpty()
+                                                                        ? implode('', $document->doneuserdocs_files($done_doc->id)->map(function($file) {
+                                                                            return "<a href='" . route('doneuserdocs.download', $file->name) . "' class='btn btn-primary'>{$file->title}</a>";
+                                                                        })->toArray())
+                                                                        : '<p>Fayl mavjud emas.</p>') . "</p>
+                                                                </div>
+                                                                <div class='modal-footer'>
+                                                                    <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Yopish</button>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>";
 
 
 
@@ -163,9 +155,6 @@
 
 
                                     </td>
-                                    <td>{!! $document->report !!}</td>
-                                    <td>{{ $document->updated_at }}</td>
-                                    <td>{{ $user->name ?? 'Mavjud emas' }}</td>
                                     <td>
                                         @if($document->get_released_order($document->id))
                                             <a href="{{ route('releasedownload', $document->get_released_order($document->id)) }}"
